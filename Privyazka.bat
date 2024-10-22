486e750657f1a231e4516b1e5001e54c0fc39f76
Mafik0 <gorbunovsemen16@gmail.com>
2024-10-22 12:07:54 +0300

    Update Privyazka.bat

--- Privyazka.bat
+++ Privyazka.bat
@@ -1,7 +1,7 @@
 @echo off
 :BegLoop
-git config --global user.name Mafik0
-git config --global user.email gorbunovsemen16@gmail.com
+git config --global user.name ""
+git config --global user.email ""
 SET /P Number=enter number
 if %number% ==- goto ExitLoop
 :ExitLoop
